b5ace201d9e0b52b2ad95726bf7b99183bdfb436
Amin <wow@WOWs-Mac.local>
2018-03-13 23:43:54 +0100

    add state for text field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-// import { View } from 'react-native';
-import { Button, Card, CardSection, Header } from './common';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+    state = { text: '' };
     render() {
         return (
             <Card>
-                <Header headerText="yooyoo" />
-                <CardSection />
+                <CardSection >
+                    <TextInput 
+                        style={{ height: 50, width: 100 }}
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                    />
+                </CardSection>
                 <CardSection />
                 <CardSection>
                     <Button>
